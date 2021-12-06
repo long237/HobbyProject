fa5282b0747e5f109fb4acdd5c70589c1b0aaf10
Im_Alpha <arcjcb11@gmail.com>
2021-12-05 23:41:27 -0800

    initial batch
    setting up sql configuration

---
+++ Source Code/TeamHobby.HobbyProjectGenerator/Implementations/Database_Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamHobby.HobbyProjectGenerator.Implementations
+{
+    internal class Database_Users : IUserService
+    {
+        public IList<string> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool User(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
