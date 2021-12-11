ba273228acdc8b2abf337b01cf9ee2b9a936e8bb
Im_Alpha <arcjcb11@gmail.com>
2021-12-10 19:48:39 -0800

    removed web template

--- Source Code/TeamHobby.HobbyProjectGenerator.Main/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TeamHobby.HobbyProjectGenerator.Main.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
