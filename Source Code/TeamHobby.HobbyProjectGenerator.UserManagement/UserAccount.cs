e6a751db313cbabb2a07931405f802872e89e5f3
Im_Alpha <arcjcb11@gmail.com>
2021-12-12 02:26:21 -0800

    Finished UserAccount Class

--- Source Code/TeamHobby.HobbyProjectGenerator.UserManagement/UserAccount.cs
+++ Source Code/TeamHobby.HobbyProjectGenerator.UserManagement/UserAccount.cs
@@ -4,31 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace main
+namespace TeamHobby.HobbyProjectGenerator.UserManagement
 {
     public class UserAccount
     {
-       /* private string username;
-        private string password;
-        private string role;
+        // Admin Credentials
+        private string UserName;
+        private string Password;
+        private DateTime Time;
 
-        public UserAccount(string un,string pwd,string rol)
+        public UserAccount(string un, string pwd, DateTime TimeStamp)
         {
-            username = un;
-            password = pwd;
-            role = rol;
-
+            UserName = un;
+            Password = pwd;
+            Time = TimeStamp;
         }
+        public string username { get { return UserName; } } 
+        public string password { get { return Password; } }
 
-        public void UserAccount()
+        // New User Credentials
+        private string newusername;
+        private string newpassword;
+        private string newemail;
+        private DateTime newtime;
+        public void NewUser(string newUN, string newPWD, string Email, DateTime newTime)
         {
-            password = "1234";
-            role = "Regular";
+            newusername = newUN;
+            newpassword = newPWD;
+            newemail = Email;
+            newtime = newTime;
         }
-
-        public void newUser()
-        {
-          
-        }*/
+        public string NewUserName { get { return newusername; } }
+        public string NewPassword { get { return newpassword; } }
+        public string NewEmail { get { return newemail; } }
+        public DateTime Newtime { get { return newtime;} }
     }
 }
